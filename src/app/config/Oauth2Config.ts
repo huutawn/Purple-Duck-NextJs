191a151cf6e47ad5125763f494167d675a2e0076
huutawn <huutankg2005@gmail.com>
2025-08-22 20:59:05 +0700

    .

--- src/app/config/Oauth2Config.ts
+++ src/app/config/Oauth2Config.ts
@@ -1,5 +1,5 @@
 export const OauthConfig = {
   clientId: "132168922366-dr6qv89fg56k81vlcu9kiag8c3abktj5.apps.googleusercontent.com", // Đây là dữ liệu public
-  redirectUri: "http://localhost:3000/authenticate",
+  redirectUri: "https://purpleduck.io.vn/authenticate",
   authUri: "https://accounts.google.com/o/oauth2/auth",
 };
